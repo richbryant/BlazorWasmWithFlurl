b61852cfe3a239efcd162bad6c0f75132b6f81a8
Rich Bryant <rich.bryant@outlook.com>
2020-08-17 13:50:36 +0100

    Removed some redundant bits

--- FlurlBlazor/Client/Program.cs
+++ FlurlBlazor/Client/Program.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Flurl.Http;
 using FlurlBlazor.Client.ApiServices;
@@ -8,16 +6,17 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace FlurlBlazor.Client
 {
-    public class Program
+    public static class Program
     {
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            FlurlHttp.Configure(settings => settings.HttpClientFactory = new BlazorHttpClientFactory());
+            FlurlHttp.Configure(settings =>
+            {
+                settings.HttpClientFactory = new BlazorHttpClientFactory();
+            });
 
             builder.Services.AddTransient<IWeatherForecastService, WeatherForecastService>();
 
